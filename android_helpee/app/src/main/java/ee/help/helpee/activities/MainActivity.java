f63585547d19d01410c86c0961a71112d45824d0
Ian Rumac <ian.rumac@infinum.hr>
2015-04-12 20:01:04 +0200

    Drawer navigation and basic gradle

---
+++ android_helpee/app/src/main/java/ee/help/helpee/activities/MainActivity.java
@@ -0,0 +1,108 @@
+package ee.help.helpee.activities;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import ee.help.helpee.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
+    PrimaryDrawerItem eventsDrawerItem;
+
+    SecondaryDrawerItem profileDrawerItem;
+
+    SecondaryDrawerItem settingsDrawerItem;
+
+    Drawer navigationDrawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        buildDrawer();
+
+
+    }
+
+
+    void buildDrawer() {
+        navigationDrawer = new Drawer().withActivity(this)
+                .withToolbar(toolbar);
+        createDrawerItems();
+        setDrawerListeners();
+
+
+
+    }
+
+    void createDrawerItems() {
+        navigationDrawer.addDrawerItems(
+                eventsDrawerItem,
+                new DividerDrawerItem(),
+                profileDrawerItem,
+                settingsDrawerItem
+
+        );
+
+
+    }
+
+    void setDrawerListeners() {
+        navigationDrawer.withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l, IDrawerItem iDrawerItem) {
+                if(iDrawerItem.equals(eventsDrawerItem)){
+                    Toast.makeText(MainActivity.this, "Events", Toast.LENGTH_SHORT).show();
+                }
+                if(iDrawerItem.equals(profileDrawerItem)){
+                    Toast.makeText(MainActivity.this, "Events", Toast.LENGTH_SHORT).show();
+                }
+                if(iDrawerItem.equals(settingsDrawerItem)){
+                    Toast.makeText(MainActivity.this, "Events", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ android_helpee/app/src/main/java/ee/help/helpee/models/Event.java
@@ -0,0 +1,11 @@
+package ee.help.helpee.models;
+
+/**
+ * Created by ian on 12/04/15.
+ */
+public class Event {
+
+
+
+
+}
